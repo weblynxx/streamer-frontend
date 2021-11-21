3f3bd6b197433f599fcc3bb98c7aaa5ddd24f9a9
weblynxx <62029041+weblynxx@users.noreply.github.com>
2021-11-21 23:19:18 +0300

    fix EditService view

--- src/components/editServices/editServices.ts
+++ src/components/editServices/editServices.ts
@@ -4,16 +4,22 @@ import { User, UserEmpty } from '../../shared/model/user';
 import { namespace } from 'vuex-class';
 import { Logger } from 'fsts';
 import Axios from 'axios';
-import {
-  getTwitchAccessToken,
-  TwitchTokenResponse,
-} from '@jlengstorf/get-twitch-oauth';
 import router from '@/router';
+import { Service } from '@/shared/model/service';
 
 const authModule = namespace('authManagement');
+const editServiceManagementModule = namespace('editServiceManagement');
 
 @Component
 export default class EditServiceComponent extends Vue {
+  @editServiceManagementModule.Action('getServices')
+  private actionGetServices!: any;
+  @editServiceManagementModule.Getter('getServices')
+  private getterServices!: Service[];
+
+  mounted() {
+    this.actionGetServices();
+  }
   private twitchOauthUrl() {
     return `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.VUE_APP_TWITCH_CLIENT_ID}&redirect_uri=http://localhost:2002/oauth/twitch&response_type=code&scope=user:read:email`;
   }
