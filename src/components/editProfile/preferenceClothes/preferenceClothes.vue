b7a46b867bc879ce1009d4281fa226bf47fecb0a
weblynxx <62029041+weblynxx@users.noreply.github.com>
2022-01-04 21:41:36 +0300

    fix editProfile view

--- src/components/editProfile/preferenceClothes/preferenceClothes.vue
+++ src/components/editProfile/preferenceClothes/preferenceClothes.vue
@@ -1,5 +1,5 @@
 <template src="./preferenceClothes.html"></template>
 
-<style scoped lang="scss" src="./preferenceClothes.scss"></style>
+<style lang="scss" src="./preferenceClothes.scss"></style>
 
 <script lang="ts" src="./preferenceClothes.ts"></script>
--- src/components/editProfile/preferenceFood/preferenceFood.vue
+++ src/components/editProfile/preferenceFood/preferenceFood.vue
@@ -1,5 +1,5 @@
 <template src="./preferenceFood.html"></template>
 
-<style scoped lang="scss" src="./preferenceFood.scss"></style>
+<style lang="scss" src="./preferenceFood.scss"></style>
 
 <script lang="ts" src="./preferenceFood.ts"></script>
